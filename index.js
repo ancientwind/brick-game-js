d1e56f07d4de94557f515bd02e17e758c06a91e3
wayne zeng <zengqw62@163.com>
2017-12-19 22:58:19 +0800

    add ball move; add collision; add start game

--- index.js
+++ index.js
@@ -8,19 +8,26 @@ var bar = {
     height: 20,
     speed: 10,
     posX: 100,
-    posY: 260,
+    posY: 270,
 }
 
 var ball = {
     width: 30,
     height: 30,
-    speed: 2,
+    speed: 5,
     posX: 100,
     posY: 150,
+    getCenterX: function() { return (this.posX + this.width/2) }
 }
 
+// for bar move
 var isKeydown = false
-var idInterval
+var idIntervalBar
+
+// for ball move
+var idIntervalBall
+var moveToLeft = true
+var moveToUp = true
 
 function initCtx() {
     canvas = document.getElementById('myGame')
@@ -51,17 +58,88 @@ function initBar() {
     barImage.src = 'img/bar.jpeg'    
 }
 
+function moveBall() {
+    idIntervalBall = window.setInterval( function() {
+        updateBallPos()
+        reDrawBall()
+    }, 1000/45)
+}
+
+function updateBallPos() {
+    // update pos x
+    if (moveToLeft) {
+        if (ball.posX <= 0) { 
+            moveToLeft = false 
+            ball.posX = 0
+        }
+        else {
+            ball.posX -= ball.speed
+        }
+    }
+    else {
+        if (ball.posX >= canvas.width - ball.width) { 
+            moveToLeft = true 
+            ball.posX = canvas.width - ball.width
+        }
+        else {
+            ball.posX += ball.speed
+        }
+    }
+
+    // update pos y
+    if (moveToUp) {
+        if (ball.posY <= 0) {
+            moveToUp = false
+            ball.posY = 0
+        }
+        else {
+            ball.posY -= ball.speed
+        }
+    }
+    else {
+        if (ball.posY >= bar.posY - ball.height) {
+            moveToUp = true
+            ball.posY = bar.posY - ball.height
+            if (!isBarTouchedBall()) {
+                window.clearInterval(idIntervalBall)
+            } 
+        }
+        else {
+            ball.posY += ball.speed
+        }
+    }
+
+}
+
+function isBarTouchedBall(){
+
+    _log('ball center x: ' + ball.getCenterX())
+    // ball.posX in [bar.posX, bar.posX+bar.width]
+    if ( ball.getCenterX() >= bar.posX && ball.getCenterX() <= bar.posX + bar.width) {
+        return true
+    }
+    // ball.posY = bar.posY , no need for this step as this function is only called when reach pos y condition
+
+    return false
+}
+
+function reDrawBall() {
+    ctx.clearRect(0,0,canvas.width, bar.posY)
+    _drawImage(ballImage, ball)
+}
+
 function bindKeyMove() {
+
     window.addEventListener('keydown', function(event) {
         if (!isKeydown) {
             isKeydown = true
-            idInterval = window.setInterval(function() {
-                moveBar(event.key) }, 1000/30) 
+            idIntervalBar = window.setInterval(function() {
+                moveBar(event.key) }, 1000/45) 
         }
     })
 
     window.addEventListener('keyup', function() {
-        window.clearInterval(idInterval)
+        window.clearInterval(idIntervalBar)
         isKeydown = false
     })
 }
@@ -98,8 +176,14 @@ function _log(text) {
     console.log(text)
 }
 
-initCtx()
-initBall()
-initBar()
-bindKeyMove()
+function _init() {
+    initCtx()
+    initBall()
+    initBar()
+}
+
+function _run() {
+    bindKeyMove()
+    moveBall()
+}
 
