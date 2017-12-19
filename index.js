2f0b7c43fb16c26c67539cbb9539844edb5dec0f
wayne zeng <zengqw62@163.com>
2017-12-19 16:56:08 +0800

    refactor; add move left

--- index.js
+++ index.js
@@ -1,44 +1,84 @@
-var ctx = document.getElementById('myGame').getContext('2d')
+var canvas
+var ctx
+var ballImage
+var barImage
 
+var barPositionX = 100
+
+var bar = {
+    width: 120,
+    height: 20,
+    speed: 1,
+    posX: 100,
+    posY: 260,
+}
+
+var ball = {
+    width: 30,
+    height: 30,
+    speed: 2,
+    posX: 100,
+    posY: 150,
+}
+
+function initCtx() {
+    canvas = document.getElementById('myGame')
+    if (canvas) {
+        ctx = canvas.getContext('2d')
+    }
+}
+
+var _drawImage = function (obj, posX, posY) {
+    if (ctx) {
+        ctx.drawImage(obj, posX, posY, ball.width, ball.height)
+    }
+};
 function initBall() {
-    var ballImage = new Image()
+    ballImage = new Image()
     ballImage.onload = function() {
-        if (ctx) {
-            ctx.drawImage(ballImage, 100, 150, 30, 30)
-        }
-    
+        _drawImage(ballImage, ball.posX, ball.posY);
     }
     ballImage.src = 'img/ball.png'
 }
 
 function initBar() {
-    var barImage = new Image()
+    barImage = new Image()
     barImage.onload = function() {
-        if (ctx) {
-            ctx.drawImage(barImage, 50, 260, 120, 20)
-        }
-    
+        _drawImage(barImage, bar.posX, bar.posY)
     }
     barImage.src = 'img/bar.jpeg'    
 }
 
+function bindKeyMove() {
+    window.addEventListener('keydown', function(event) {
+        if (event.key === 'a') {
+            moveLeft()
+        }
+        else if (event.key === 'd') {
+            moveRight()
+        }
+    })
+}
+
+function clearBar() {
+    ctx.clearRect(0, 260, canvas.width, bar.height)
+}
 function moveLeft() {
-    var event = window.event
-    _log(event) 
+    clearBar()
+    _drawImage(barImage, --bar.posX, bar.posY)
+    _log('move left' + bar.posX)
 }
 
 function moveRight() {
-
+    _log('move right')
 }
 
 function _log(text) {
     console.log(text)
 }
 
+initCtx()
 initBall()
 initBar()
+bindKeyMove()
 
-window.addEventListener('keydown', function(event) {
-    _log('key pressed')
-    _log(event.key)
-})
